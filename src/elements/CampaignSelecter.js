de6cbbb31474a38c69c45628c28ae610bdf37f93
Unknown <nemirovsky.nirel@gmail.com>
2017-04-03 23:16:02 +0300

    Added limit

--- src/elements/CampaignSelecter.js
+++ src/elements/CampaignSelecter.js
@@ -13,6 +13,7 @@ class CampaignSelecter extends Component {
     super();
     this.state = {
       selectFieldValue: 1,
+      limitValue: 100,
       campgianNames: [],
       limit: 0,
     }
@@ -23,6 +24,7 @@ class CampaignSelecter extends Component {
   }
 
  handleChange = (event, index, selectFieldValue) => this.setState({selectFieldValue});
+ handleLimitChange = (event, index, limitValue) => this.setState({limitValue});
 
  getAllCampaignNames = () => {
    let storedCampaigns = JSON.parse(localStorage.getItem("campaigns"));
@@ -35,7 +37,7 @@ class CampaignSelecter extends Component {
 
  sendStart = () => {
    let selectedCampaign = JSON.parse(localStorage.getItem("campaigns"))[this.state.selectFieldValue];
-   connectionHandler.startAlgo(selectedCampaign, 10)
+   connectionHandler.startAlgo(selectedCampaign, this.state.limitValue)
    .then(function(){
      console.log('sent to start');
    })
@@ -95,6 +97,24 @@ class CampaignSelecter extends Component {
               this.state.campaignNames.map((name, index) =>
               <MenuItem key={name} value={index} primaryText={name} />
             )}
+         </SelectField>
+       </div>
+
+       <div style={{marginLeft: '40%'}}>
+         <SelectField
+            floatingLabelText="limis"
+            value={this.state.limitValue}
+            onChange={this.handleLimitChange}
+          >
+            <MenuItem key={100} value={100} primaryText={100} />
+            <MenuItem key={200} value={200} primaryText={200} />
+            <MenuItem key={300} value={300} primaryText={300} />
+            <MenuItem key={400} value={400} primaryText={400} />
+            <MenuItem key={500} value={500} primaryText={500} />
+            <MenuItem key={600} value={600} primaryText={600} />
+            <MenuItem key={700} value={700} primaryText={700} />
+            <MenuItem key={800} value={800} primaryText={800} />
+
          </SelectField>
        </div>
 
